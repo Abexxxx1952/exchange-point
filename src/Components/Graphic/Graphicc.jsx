6e1caec61d285e7106c2a09c6f9b0774bc93b6bb
Abexxxx1952 <103738739+Abexxxx1952@users.noreply.github.com>
2022-04-17 20:03:39 +0300

    graphic done

---
+++ src/Components/Graphic/Graphicc.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { ResponsiveLine } from "@nivo/line";
+
+const Graphicc = ({}) => {
+  return (
+    <ResponsiveLine
+      data={data}
+      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      xScale={{ type: "point" }}
+      yScale={{
+        type: "linear",
+        min: "auto",
+        max: "auto",
+        stacked: true,
+        reverse: false,
+      }}
+      yFormat=" >-.2f"
+      curve="catmullRom"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        orient: "bottom",
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "transportation",
+        legendOffset: 36,
+        legendPosition: "middle",
+      }}
+      axisLeft={{
+        orient: "left",
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "count",
+        legendOffset: -40,
+        legendPosition: "middle",
+      }}
+      enableGridX={false}
+      pointSize={10}
+      pointColor={{ theme: "background" }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: "serieColor" }}
+      pointLabelYOffset={-12}
+      enableArea={true}
+      areaOpacity={0.65}
+      useMesh={true}
+      legends={[
+        {
+          anchor: "top",
+          direction: "row",
+          justify: false,
+          translateX: 162,
+          translateY: 7,
+          itemsSpacing: 0,
+          itemDirection: "left-to-right",
+          itemWidth: 80,
+          itemHeight: 20,
+          itemOpacity: 0.75,
+          symbolSize: 12,
+          symbolShape: "circle",
+          symbolBorderColor: "rgba(0, 0, 0, .5)",
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemBackground: "rgba(0, 0, 0, .03)",
+                itemOpacity: 1,
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  );
+};
+
+export default Graphicc;
